6d3a53e882a84c38fa01ec17887cb361add64169
andrusha10t500 <andrusha10t500@gmail.com>
2019-05-19 21:32:12 +1000

    remove

--- in_DB.sh
+++ in_DB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in ./images/*; do 
-mysql -u root -p"qt67uy" mix -e "INSERT INTO mix_podelki (photo) VALUES ('$i')"
+mysql -u root -p"password" mix -e "INSERT INTO mix_podelki (photo) VALUES ('$i')"
 done
